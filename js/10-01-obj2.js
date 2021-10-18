8e0bdf2a0d7f504ee7ff845b2f0f57d7e526457a
Luis M Canaval S <public02@canaval.org>
2021-10-18 14:55:23 -0500

    cleaning up a little

--- js/10-01-obj2.js
+++ js/10-01-obj2.js
@@ -6,27 +6,20 @@ async function main() {
   const gl = document.querySelector("#canvitas").getContext("webgl2");
   if (!gl) return undefined !== console.log("couldn't create webgl2 context");
 
+  twgl.setAttributePrefix("a_");
+
+  // Loading monito
   const vertfn = "glsl/10-01.vert";
   const fragfn = "glsl/10-01.frag";
   const objfn = "objects/monito/monito.obj";
-  const lsvertfn = "glsl/09-01-ls.vert";
-  const lsfragfn = "glsl/09-01-ls.frag";
-  const lsfn = "objects/cubito/cubito.obj";
-
-  twgl.setAttributePrefix("a_");
 
   const vertSrc = await fetch(vertfn).then((resp) => resp.text());
   const fragSrc = await fetch(fragfn).then((resp) => resp.text());
   const objText = await fetch(objfn).then((resp) => resp.text());
-  const lsvertSrc = await fetch(lsvertfn).then((resp) => resp.text());
-  const lsfragSrc = await fetch(lsfragfn).then((resp) => resp.text());
-  const lsText = await fetch(lsfn).then((resp) => resp.text());
 
   const meshProgramInfo = twgl.createProgramInfo(gl, [vertSrc, fragSrc]);
-  const lsProgramInfo = twgl.createProgramInfo(gl, [lsvertSrc, lsfragSrc]);
 
   const obj = cg.parseObj(objText);
-  const lsCube = cg.parseObj(lsText);
 
   const baseHref = new URL(objfn, window.location.href);
   const matTexts = await Promise.all(obj.materialLibs.map(async (filename) => {
@@ -51,6 +44,19 @@ async function main() {
       vao,
     };
   });
+
+  // loading light source cube
+  const lsvertfn = "glsl/09-01-ls.vert";
+  const lsfragfn = "glsl/09-01-ls.frag";
+  const lsfn = "objects/cubito/cubito.obj";
+
+  const lsvertSrc = await fetch(lsvertfn).then((resp) => resp.text());
+  const lsfragSrc = await fetch(lsfragfn).then((resp) => resp.text());
+  const lsText = await fetch(lsfn).then((resp) => resp.text());
+
+  const lsProgramInfo = twgl.createProgramInfo(gl, [lsvertSrc, lsfragSrc]);
+
+  const lsCube = cg.parseObj(lsText);
   const lsParts = lsCube.geometries.map(({ data }) => {
     const bufferInfo = twgl.createBufferInfoFromArrays(gl, data);
     const vao = twgl.createVAOFromBufferInfo(gl, meshProgramInfo, bufferInfo);
@@ -60,6 +66,7 @@ async function main() {
     };
   });
 
+  // General stuff setup
   const cam = new cg.Cam([0, 1.5, 6]);
   const rotationAxis = new Float32Array([0, 1, 0]);
 
@@ -80,11 +87,14 @@ async function main() {
   gl.enable(gl.DEPTH_TEST);
   gl.enable(gl.CULL_FACE);
 
+  // Render awesome
   function render(elapsedTime) {
+    // handling time in seconds maybe
     elapsedTime *= 1e-3;
     deltaTime = elapsedTime - lastTime;
     lastTime = elapsedTime;
 
+    // resizing stuff and general preparation
     if (twgl.resizeCanvasToDisplaySize(gl.canvas)) {
       gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
       aspect = gl.canvas.width / gl.canvas.height;
@@ -92,16 +102,19 @@ async function main() {
     gl.clearColor(0.1, 0.1, 0.1, 1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+    // some logic to move the light around
     theta = elapsedTime;
     mat4.identity(uniforms.u_world);
-		mat4.rotate(uniforms.u_world, uniforms.u_world, theta, rotationAxis);
+    mat4.rotate(uniforms.u_world, uniforms.u_world, theta, rotationAxis);
     mat4.translate(uniforms.u_world, uniforms.u_world, [1.75, 0, 0]);
     vec3.transformMat4(uniforms.u_light_position, origin, uniforms.u_world);
 
+    // coordinate system adjustments
     mat4.identity(uniforms.u_projection);
     mat4.perspective(uniforms.u_projection, cam.zoom, aspect, 0.1, 100);
     mat4.identity(uniforms.u_world);
 
+    // drawing monito
     gl.useProgram(meshProgramInfo.program);
     twgl.setUniforms(meshProgramInfo, uniforms);
 
@@ -110,6 +123,7 @@ async function main() {
       twgl.drawBufferInfo(gl, bufferInfo);
     }
 
+    // logic to move the visual representation of the light source
     mat4.identity(uniforms.u_world);
     mat4.translate(
       uniforms.u_world,
@@ -117,8 +131,11 @@ async function main() {
       uniforms.u_light_position,
     );
     mat4.scale(uniforms.u_world, uniforms.u_world, [0.05, 0.05, 0.05]);
+
+    // drawing the light source cube
     gl.useProgram(lsProgramInfo.program);
     twgl.setUniforms(lsProgramInfo, uniforms);
+
     for (const { bufferInfo, vao } of lsParts) {
       gl.bindVertexArray(vao);
       twgl.drawBufferInfo(gl, bufferInfo);
